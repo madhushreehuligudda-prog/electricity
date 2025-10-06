356f127ef3afb40e567ba730a7cd028367ace223
madhushreehuligudda-prog <madhushreehuligudda@gmail.com>
2025-10-06 17:26:21 +0530

    Create bill.py

---
+++ bill.py
@@ -0,0 +1,5 @@
+rate_per_unit = 75
+units = int(input("Enter the number of units consumed: "))
+total_bill = units * rate_per_unit
+print(f"Units Consumed: {units}")
+print(f"Total Bill: ₹{total_bill}")
